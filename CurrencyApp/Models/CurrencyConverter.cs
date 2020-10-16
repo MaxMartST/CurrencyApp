916b3d8069131257e16e5754d5c57e7c4f06ad4c
Max <addergtx@gmail.com>
2020-10-16 21:03:37 +0300

    Add project files.

---
+++ CurrencyApp/Models/CurrencyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyApp.Models
+{
+    public class CurrencyConverter
+    {
+        public decimal USD { get; set; }
+        public decimal EUR { get; set; }
+        public decimal UAH { get; set; }
+
+        public decimal ConvertToUSD(decimal priceRUB) => priceRUB / USD;
+        public decimal ConvertToEUR(decimal priceRUB) => priceRUB / EUR;
+        public decimal ConvertToUAH(decimal priceRUB) => priceRUB / (UAH / 10);
+
+    }
+}
---
+++ CurrencyApp/Models/CurrencyService.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CurrencyApp.Models
+{
+    public class CurrencyService : BackgroundService
+    {
+        //создадим кеш, чтобы хранить в нём данные с api банка
+        private readonly IMemoryCache memoryCache;
+        public CurrencyService(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //пока не было запроса на остановку нашей задачи, она будет выполняться
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                try
+                {
+                    Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+
+                    Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+                    XDocument xml = XDocument.Load("http://www.cbr.ru/scripts/XML_daily.asp");
+
+                    CurrencyConverter currencyConverter = new CurrencyConverter();
+                   
+                    currencyConverter.USD = Convert.ToDecimal(xml.Elements("ValCurs").Elements("Valute")
+                        .FirstOrDefault(x => x.Element("NumCode").Value == "840").Elements("Value").FirstOrDefault().Value);
+                    
+                    currencyConverter.EUR = Convert.ToDecimal(xml.Elements("ValCurs").Elements("Valute")
+                        .FirstOrDefault(x => x.Element("NumCode").Value == "978").Elements("Value").FirstOrDefault().Value);
+                    
+                    currencyConverter.UAH = Convert.ToDecimal(xml.Elements("ValCurs").Elements("Valute")
+                        .FirstOrDefault(x => x.Element("NumCode").Value == "840").Elements("Value").FirstOrDefault().Value);
+
+                    memoryCache.Set("key_currency", currencyConverter, TimeSpan.FromMinutes(14444));
+                }
+                catch (Exception e)
+                { 
+                    //logs...
+                }
+
+                await Task.Delay(3600000, stoppingToken);
+            }
+        }
+    }
+}
